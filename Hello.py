fd8b2624278ad318fadd892f0c4e82d14db8d823
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-29 23:22:02 +0000

    init commit

---
+++ Hello.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(
+    page_title = "Hello"
+    page_icon= ""
+)
+
+expander = st.expander("Domain knowledge of Oil & Gas")
+
+expander.write("""
+Hello World!""")
