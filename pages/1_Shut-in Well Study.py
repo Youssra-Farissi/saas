0955513524df9416e47b84cdea28f7bc912c0715
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-30 19:41:04 +0000

    1_shut-in well Study.py

--- pages/1_Shut-in Well Study.py
+++ pages/1_Shut-in Well Study.py
@@ -1,23 +1,22 @@
 import streamlit as st
-import pandas as pd
 
 def main():
     st.title("Téléchargement de données")
 
     st.write("Veuillez télécharger votre fichier Excel.")
 
+    # Utilisez st.file_uploader avec le paramètre `type` pour spécifier les extensions autorisées
     data_uploader = st.file_uploader("Télécharger fichier Excel (xlsx)", type=["xlsx"])
 
     if data_uploader is not None:
         st.success("Fichier téléchargé avec succès!")
         data_uploader.seek(0)
         try:
-            # Utiliser 'latin-1' pour décoder les données
-            string = data_uploader.read().decode("latin-1")
-            temp_df1 = pd.read_excel(string)
-
-            # Votre code de traitement des données ici
-            # Par exemple:
+            string = data_uploader.read().decode("utf-8")
+            log = ls.read(string)
+            temp_df1 = log.df()
+            temp_df1 = temp_df1.reset_index()
+            temp_df1.columns
             temp_df1.rename(columns={'DEPT':'DEPTH','SGRC':'GR','TNPL':'NPHI','PEF':'PE',
                                      'HSI':'CALI','SBD2':'RHOB','PRES2M16IN':'RS','PRES500K48IN':'RT','STOP':'ROP',
                                      'SFXE':'FEXP'}, inplace=True)
