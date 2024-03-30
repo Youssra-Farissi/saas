1cf49a65b66cff09ab0f774c835b8b0ab1b1775e
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-30 18:46:08 +0000

    1_shut-in well Study.py

--- pages/1_Shut-in Well Study.py
+++ pages/1_Shut-in Well Study.py
@@ -18,7 +18,7 @@ def main():
         temp_df1=log.df()
         temp_df1=temp_df1.reset_index()
         temp_df1.columns
-        temp_df1.rename(columns={'DEPT:DEPTH','SGRC':'GR','TNPL':'NPHI','PEF':'PE','HSI':'CALI','SBD2':'RHOB','PRES2M16IN':'RS','PRES500K48IN':'RT','STOP':'ROP',
+        temp_df1.rename(columns={'DEPT':'DEPTH','SGRC':'GR','TNPL':'NPHI','PEF':'PE','HSI':'CALI','SBD2':'RHOB','PRES2M16IN':'RS','PRES500K48IN':'RT','STOP':'ROP',
         'SFXE':'FEXP'},inplace=true)
         temp_df1=temp_df1.dropna()
        
