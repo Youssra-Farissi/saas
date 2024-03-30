1cb573162deea0d11c89d54c6bc8175c1ac27f8a
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-30 19:49:16 +0000

    1_shut-in well Study.py

--- pages/1_Shut-in Well Study.py
+++ pages/1_Shut-in Well Study.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import ls  # Import the ls module if it's a custom module used for reading Excel files
+import pandas as pd
 
 def main():
     st.title("Téléchargement de données")
-
     st.write("Veuillez télécharger votre fichier Excel.")
 
     data_uploader = st.file_uploader("Télécharger fichier Excel (xlsx)", type=["xlsx"])
@@ -12,19 +11,19 @@ def main():
         try:
             st.success("Fichier téléchargé avec succès!")
             data_uploader.seek(0)
-            string = data_uploader.read()  # Don't specify decode type
-            log = ls.read(string)  # Assuming ls is a module for reading Excel files
-            temp_df1 = log.df()
-            temp_df1 = temp_df1.reset_index()
-            temp_df1.columns
+            # Read the Excel file using pandas
+            temp_df1 = pd.read_excel(data_uploader)
+            # Rename columns
             temp_df1.rename(columns={'DEPT':'DEPTH','SGRC':'GR','TNPL':'NPHI','PEF':'PE',
                                      'HSI':'CALI','SBD2':'RHOB','PRES2M16IN':'RS','PRES500K48IN':'RT','STOP':'ROP',
                                      'SFXE':'FEXP'}, inplace=True)
+            # Drop rows with missing values
             temp_df1 = temp_df1.dropna()
-          
+            # Select desired columns
             temp_df1 = temp_df1[['DEPTH', 'ROP', 'GR', 'NPHI', 'PE', 'CALI', 'RHOB', 'RS', 'RT', 'ROP', 'FEXP', 'RPM']]
+            # Calculate standard deviations and means
             std_cali = temp_df1['CALI'].std()
-            std_gr = temp_df1["GR"].std()
+            std_gr = temp_df1['GR'].std()
             std_p = temp_df1['NPHI'].mean()
             std_d = temp_df1['RHOB'].mean()
             mean_r = temp_df1['RT'].mean()
