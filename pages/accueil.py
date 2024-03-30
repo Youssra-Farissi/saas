45870a1be3407245249f2ae6e1e0fa6100564277
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-30 17:28:55 +0000

    accueil.py

---
+++ pages/accueil.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Bienvenue sur notre application")
+
+    st.write("Cette application est destinée à la visualisation de données pétrolières et gazières.")
+
+    st.write("Pour commencer, veuillez télécharger vos données dans la section 'Téléchargement de données'.")
+
+if __name__ == "__main__":
+    main()
