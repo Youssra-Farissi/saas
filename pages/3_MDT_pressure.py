f581b6483b91c8e37a56ee9512f0a53aa6fb6b3f
FARISSI Youssra <104703422+Youssra-Farissi@users.noreply.github.com>
2024-03-30 20:04:30 +0000

    3_MDT_pressure.py

---
+++ pages/3_MDT_pressure.py
@@ -0,0 +1,5 @@
+import scipy
+from scipy import spatial 
+dis_r=spatial.distance.cdist(
+      d_list[i]['RS'].values.reshape ( -1 ,len( d_list [ i ] ['RS' ] .values))
+      .d_list[i]['RT'].values.reshape ( -1 ,len( d_list [ i ] ['Rt' ] .values))
